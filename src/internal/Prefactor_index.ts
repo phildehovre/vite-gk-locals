741c58d008e50ac1ab0e2cefd3b4347c3bc314c0
phildehovre <66724307+phildehovre@users.noreply.github.com>
2023-03-28 12:30:04 +0100

    tezst 12 29

--- src/internal/Prefactor_index.ts
+++ src/internal/Prefactor_index.ts
@@ -1,56 +1,56 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
-import * as express from "express";
-import * as SibApiV3Sdk from "sib-api-v3-sdk";
-import {config} from "dotenv";
-import * as cors from "cors";
+// import * as functions from "firebase-functions";
+// import * as admin from "firebase-admin";
+// import * as express from "express";
+// import * as SibApiV3Sdk from "sib-api-v3-sdk";
+// import {config} from "dotenv";
+// import * as cors from "cors";
 
 
-config();
+// config();
 
-const app = express();
-admin.initializeApp();
+// const app = express();
+// admin.initializeApp();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+// // Start writing functions
+// // https://firebase.google.com/docs/functions/typescript
 
-const defaultClient = SibApiV3Sdk.ApiClient.instance;
-const apiKey = defaultClient.authentications["api-key"];
-apiKey.apiKey = process.env.VITE_SENDINBLUE_API_KEY;
+// const defaultClient = SibApiV3Sdk.ApiClient.instance;
+// const apiKey = defaultClient.authentications["api-key"];
+// apiKey.apiKey = process.env.VITE_SENDINBLUE_API_KEY;
 
-const sendinblue = (sendSmtpEmail: any) => {
-  const transactionnalApiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+// const sendinblue = (sendSmtpEmail: any) => {
+//   const transactionnalApiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
-  transactionnalApiInstance.sendTransacEmail(sendSmtpEmail).then(
-    function(data: any) {
-      console.log(data);
-      return true;
-    },
-    function(error: any) {
-      console.log(error);
-      return false;
-    });
-};
-
-
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
-
-app.post("/sendMail", async (req, res) => {
-  const sendSmtpEmail = {
-    to: [
-      {email: req.body.recipientMail},
-    ],
-    templateId: req.body.templateId,
-    params: req.body.templateParams,
-  };
-  sendinblue(sendSmtpEmail);
-
-  res.status(200).send({mailSent: "success"});
-});
+//   transactionnalApiInstance.sendTransacEmail(sendSmtpEmail).then(
+//     function(data: any) {
+//       console.log(data);
+//       return true;
+//     },
+//     function(error: any) {
+//       console.log(error);
+//       return false;
+//     });
+// };
 
 
-exports.api = functions.https.onRequest(app);
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+// app.post("/sendMail", async (req, res) => {
+//   const sendSmtpEmail = {
+//     to: [
+//       {email: req.body.recipientMail},
+//     ],
+//     templateId: req.body.templateId,
+//     params: req.body.templateParams,
+//   };
+//   sendinblue(sendSmtpEmail);
+
+//   res.status(200).send({mailSent: "success"});
+// });
+
+
+// exports.api = functions.https.onRequest(app);
 
